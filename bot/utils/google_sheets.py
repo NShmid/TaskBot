8a5248879bfcd249e663455b7a1618cbbe58ffb5
Nik <nikitasmidt01@gmail.com>
2025-03-11 13:37:45 +0700

    Добавлен файл для зависимостей; изменен доступ к кредитам

--- bot/utils/google_sheets.py
+++ bot/utils/google_sheets.py
@@ -1,6 +1,15 @@
+import os
+import json
 import gspread
+from dotenv import find_dotenv, load_dotenv
 
-gc = gspread.service_account("creds.json")
+
+load_dotenv(find_dotenv())
+
+creds_json = os.getenv('GSPREAD_CREDS')
+creds_dict = json.loads(creds_json)
+
+gc = gspread.service_account_from_dict(creds_dict)
 admin_sheet = gc.open("TaskBotAdmins").sheet1
 
 admins_id = set(admin_sheet.col_values(1)[1:])
